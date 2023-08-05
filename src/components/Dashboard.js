8c5ee9fe082222b177e1388b85f1a04c2ae94409
Hussain <hussainabuwala.1997@gmail.com>
2023-08-04 22:48:05 -0400

    added firebstore and firebase storage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,60 +1,77 @@
-import React, { useState } from "react";
-import { Card, Button, Alert } from "react-bootstrap";
-import { useAuth } from "../contexts/AuthContext";
+import React, { useState, useEffect } from "react";
 import "./Dashboard.css";
+import { db } from "../firebase";
+import { useAuth } from "../contexts/AuthContext";
+import {
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
 
 export default function Dashboard() {
-  const initialData = [
-    {
-      id: 1,
-      company: "Apple",
-      position: "Software Engineer",
-      stage: "Applied",
-      dueDate: "2023-08-15",
-      resume: "",
-    },
-    {
-      id: 2,
-      company: "Google",
-      position: "Web Developer",
-      stage: "Interviewed",
-      dueDate: "2023-08-20",
-      resume: "",
-    },
-    {
-      id: 3,
-      company: "Microsoft",
-      position: "Full Stack Developer",
-      stage: "Interviewed",
-      dueDate: "2023-08-20",
-      resume: "",
-    },
-    {
-      id: 4,
-      company: "Facebook",
-      position: "ML Engineer",
-      stage: "Interviewed",
-      dueDate: "2023-08-20",
-      resume: "",
-    },
-    // Add more data as needed
-  ];
-
-  const [data, setData] = useState(initialData);
+  const { currentUser } = useAuth();
+  const [data, setData] = useState([]);
   const [editCell, setEditCell] = useState({ rowId: null, field: null });
   const [showDeleteButton, setShowDeleteButton] = useState(false);
+  const storage = getStorage();
+
+  useEffect(() => {
+    async function fetchData() {
+      const userId = currentUser.uid; // Replace this with the user ID you want to match
+
+      // Create a reference to the collection you want to query
+      const usersCollectionRef = collection(db, "users");
+
+      // Create a query to filter documents based on the user ID field
+      const q = query(usersCollectionRef, where("id", "==", userId));
+
+      try {
+        const querySnapshot = await getDocs(q);
+        let allUserData = [];
+        querySnapshot.forEach((doc) => {
+          console.log("Document Retrieved ID:", doc.id);
+          let newData = doc.data();
+          newData.docID = doc.id;
+          allUserData.push(newData);
+        });
+        setData(allUserData);
+      } catch (error) {
+        console.error("Error getting documents:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   const handleCellClick = (rowId, field) => {
     setEditCell({ rowId, field });
   };
 
-  const handleCellBlur = () => {
+  async function handleCellBlur(item) {
+    const userDocRef = doc(db, "users", item.docID);
+    let newData = { ...item };
+    delete newData.docID;
+
+    // Using async/await
+    try {
+      await updateDoc(userDocRef, newData);
+      console.log("Document updated successfully");
+    } catch (error) {
+      console.error("Error updating document: ", error);
+    }
+
     setEditCell({ rowId: null, field: null });
-  };
+  }
 
   const handleCellValueChange = (event, rowId, field) => {
     const updatedData = data.map((item) =>
-      item.id === rowId ? { ...item, [field]: event.target.value } : item
+      item.docID === rowId ? { ...item, [field]: event.target.value } : item
     );
     setData(updatedData);
   };
@@ -69,25 +86,46 @@ export default function Dashboard() {
     setEditCell({ rowId: null, field: null });
   }
 
-  function handleDelete(e, rowId) {
-    const newData = data.filter((item) => item.id !== rowId);
+  async function handleDelete(item) {
+    const newData = data.filter((record) => record.docID !== item.docID);
+    const documentId = item.docID; // Replace this with the ID of the document you want to delete
+
+    // Create a reference to the document you want to delete
+    const documentRef = doc(db, "users", documentId);
+
+    // Using async/await to delete the document
+    try {
+      await deleteDoc(documentRef);
+      console.log("Document successfully deleted.");
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+    }
     setData(newData);
   }
 
-  function addRecord(e) {
-    const emptyRecord = {
-      id: 5,
+  async function addRecord(e) {
+    let emptyRecord = {
+      id: currentUser.uid,
       company: "",
       position: "",
       stage: "",
       dueDate: "",
-      resume: "",
+      resume: [],
     };
+
+    try {
+      const docRef = await addDoc(collection(db, "users"), emptyRecord);
+      console.log("Document written with ID: ", docRef.id);
+      emptyRecord.docID = docRef.id;
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+
     const newData = [...data, emptyRecord];
     setData(newData);
   }
 
-  function handleResumeChange(e, rowId, field) {
+  function handleResumeChange(e, record, field) {
     const selectedFile = e.target.files[0];
 
     if (selectedFile) {
@@ -97,10 +135,54 @@ export default function Dashboard() {
       console.log("File Type:", selectedFile.type);
     }
 
-    const updatedData = data.map((item) =>
-      item.id === rowId ? { ...item, [field]: selectedFile.name } : item
-    );
-    setData(updatedData);
+    if (selectedFile) {
+      const path = `pdfs/${currentUser.uid}/${record.docID}/${selectedFile.name}`;
+      const storageRef = ref(storage, path);
+      const fileRef = ref(storage, path);
+
+      uploadBytes(storageRef, selectedFile)
+        .then(() => {
+          console.log("PDF uploaded successfully.");
+          getDownloadURL(fileRef)
+            .then(async (url) => {
+              const updatedData = data.map((item) =>
+                item.docID === record.docID
+                  ? {
+                      ...item,
+                      [field]: [
+                        ...item.resume,
+                        { filename: selectedFile.name, downloadURL: url },
+                      ],
+                    }
+                  : item
+              );
+              let newRecord = { ...record };
+              newRecord.resume.push({
+                filename: selectedFile.name,
+                downloadURL: url,
+              });
+
+              const userDocRef = doc(db, "users", record.docID);
+              delete newRecord.docID;
+
+              // Using async/await
+              try {
+                await updateDoc(userDocRef, newRecord);
+                console.log("Document updated successfully");
+              } catch (error) {
+                console.error("Error updating document: ", error);
+              }
+
+              setData(updatedData);
+            })
+            .catch((error) => {
+              console.error("Error getting download URL:", error);
+            });
+        })
+        .catch((error) => {
+          console.error("Error uploading PDF:", error);
+        });
+    }
   }
 
   const sortDataByField = (fieldName, order) => {
@@ -135,12 +217,48 @@ export default function Dashboard() {
       stageClass = "in-progress";
     } else if (stage === "Signed" || stage === "Offered") {
       stageClass = "complete";
-    }
-    else{
-      stageClass = "rejected"
+    } else {
+      stageClass = "rejected";
     }
     stageClass += " stage-tag";
-    return stageClass
+    return stageClass;
+  }
+
+  async function handleFileDelete(record, filename) {
+    const updatedData = data.map((item) =>
+      item.docID === record.docID
+        ? {
+            ...item,
+            resume: item.resume.filter((obj) => obj.filename !== filename),
+          }
+        : item
+    );
+
+    const path = `pdfs/${currentUser.uid}/${record.docID}/${filename}`
+    const fileRef = ref(storage, path);
+
+    deleteObject(fileRef)
+      .then(() => {
+        console.log("File deleted successfully.");
+      })
+      .catch((error) => {
+        console.error("Error deleting file:", error);
+      });
+    
+    let newRecord = {...record, resume: record.resume.filter((obj) => obj.filename !== filename)}
+    delete newRecord.docID;
+
+    const userDocRef = doc(db, "users", record.docID);    
+
+    // Using async/await
+    try {
+      await updateDoc(userDocRef, newRecord);
+      console.log("Document updated successfully");
+    } catch (error) {
+      console.error("Error updating document: ", error);
+    }
+    
+    setData(updatedData)
   }
 
   return (
@@ -206,66 +324,72 @@ export default function Dashboard() {
         </thead>
         <tbody>
           {data.map((item) => (
-            <tr key={item.id}>
+            <tr key={item.docID}>
               <td
-                onMouseEnter={(e) => handleMouseEnter(e, item.id)}
+                onMouseEnter={(e) => handleMouseEnter(e, item.docID)}
                 onMouseLeave={(e) => handleMouseLeave()}
                 className="delete-cell"
               >
-                {editCell.rowId === item.id && showDeleteButton && (
+                {editCell.rowId === item.docID && showDeleteButton && (
                   <i
-                    onClick={(e) => handleDelete(e, item.id)}
+                    onClick={(e) => handleDelete(item)}
                     className="bi bi-trash"
                     style={{ cursor: "pointer" }}
                   ></i>
                 )}
               </td>
               <td
-                onClick={() => handleCellClick(item.id, "company")}
-                onBlur={handleCellBlur}
+                onClick={() => handleCellClick(item.docID, "company")}
+                onBlur={(e) => handleCellBlur(item)}
               >
-                {editCell.rowId === item.id && editCell.field === "company" ? (
+                {editCell.rowId === item.docID &&
+                editCell.field === "company" ? (
                   <input
                     type="text"
                     value={item.company}
                     onChange={(e) =>
-                      handleCellValueChange(e, item.id, "company")
+                      handleCellValueChange(e, item.docID, "company")
                     }
                     autoFocus
                   />
                 ) : (
-                  <i class="bi bi-building">{item.company}</i>
+                  item.company ? <i className="bi bi-building">{item.company}</i> : ""
                 )}
               </td>
               <td
-                onClick={() => handleCellClick(item.id, "position")}
-                onBlur={handleCellBlur}
+                onClick={() => handleCellClick(item.docID, "position")}
+                onBlur={(e) => handleCellBlur(item)}
               >
-                {editCell.rowId === item.id && editCell.field === "position" ? (
+                {editCell.rowId === item.docID &&
+                editCell.field === "position" ? (
                   <input
                     type="text"
                     value={item.position}
                     onChange={(e) =>
-                      handleCellValueChange(e, item.id, "position")
+                      handleCellValueChange(e, item.docID, "position")
                     }
                     autoFocus
                   />
                 ) : (
-                  <span className="position-container">{item.position}</span>
+                  item.position ? <span className="position-container">{item.position}</span> : ""
                 )}
               </td>
               <td
-                onClick={() => handleCellClick(item.id, "stage")}
-                onBlur={handleCellBlur}
+                onClick={() => handleCellClick(item.docID, "stage")}
+                onBlur={(e) => handleCellBlur(item)}
               >
-                {editCell.rowId === item.id && editCell.field === "stage" ? (
+                {editCell.rowId === item.docID && editCell.field === "stage" ? (
                   <select
                     value={item.stage}
-                    onChange={(e) => handleCellValueChange(e, item.id, "stage")}
+                    onChange={(e) =>
+                      handleCellValueChange(e, item.docID, "stage")
+                    }
                     autoFocus
                   >
                     <option value="Ready to apply">Ready to apply</option>
-                    <option value="Messaged recruiter">Messaged recruiter</option>
+                    <option value="Messaged recruiter">
+                      Messaged recruiter
+                    </option>
                     <option value="Referred">Referred</option>
                     <option value="Applied">Applied</option>
                     <option value="Followed up">Followed up</option>
@@ -275,46 +399,58 @@ export default function Dashboard() {
                     <option value="Rejected">Rejected</option>
                   </select>
                 ) : (
-                  <span className={getStageClass(item.stage)}>{item.stage}</span>
+                  item.stage ? <span className={getStageClass(item.stage)}> {item.stage} </span> : ""
                 )}
               </td>
               <td
-                onClick={() => handleCellClick(item.id, "dueDate")}
-                onBlur={handleCellBlur}
+                onClick={() => handleCellClick(item.docID, "dueDate")}
+                onBlur={(e) => handleCellBlur(item)}
               >
-                {editCell.rowId === item.id && editCell.field === "dueDate" ? (
+                {editCell.rowId === item.docID &&
+                editCell.field === "dueDate" ? (
                   <input
                     type="date"
                     value={item.dueDate}
                     onChange={(e) =>
-                      handleCellValueChange(e, item.id, "dueDate")
+                      handleCellValueChange(e, item.docID, "dueDate")
                     }
                     autoFocus
                   />
                 ) : (
-                  <i class="bi bi-calendar"> {item.dueDate} </i>
+                  item.dueDate ? <i className="bi bi-clock-fill"> {item.dueDate} </i> : ""
                 )}
               </td>
-              <td>
-                {editCell.rowId === item.id && editCell.field === "resume" ? (
+              <td onMouseLeave={(e) => handleCellBlur(item)} className="resume-cell">
+                {editCell.rowId === item.docID &&
+                editCell.field === "resume" ? (
                   <div>
                     <input
                       type="file"
-                      onChange={(e) => handleResumeChange(e, item.id, "resume")}
+                      onChange={(e) => handleResumeChange(e, item, "resume")}
                       accept=".pdf,.docx"
                     />
-                    {item.resume && (
-                      <button className="btn btn-link">{item.resume}</button>
-                    )}
+                    {item.resume.map((file, index) => (
+                      <div key={index}>
+                        <a target="_blank" rel="noreferrer" href={file.downloadURL}>
+                          {file.filename}
+                        </a>{" "}
+                        <i
+                          className="bi bi-x-square"
+                          onClick={(e) => handleFileDelete(item, file.filename)}
+                          style={{ cursor: "pointer" }}
+                        ></i>
+                      </div>
+                    ))}
                   </div>
                 ) : (
-                  <button
-                    className="btn btn-link"
-                    onClick={() => handleCellClick(item.id, "resume")}
-                    onBlur={handleCellBlur}
+                  <div
+                    onClick={() => handleCellClick(item.docID, "resume")}
                   >
-                    {item.resume ? item.resume : "Upload Resume"}
-                  </button>
+                    {item.resume.length === 0 ? <br></br> : ""}
+                    {item.resume.map((file, index) => (
+                      <div className="resume-items" key={index}>{file.filename}</div>
+                    ))}
+                  </div>
                 )}
               </td>
             </tr>
@@ -322,7 +458,7 @@ export default function Dashboard() {
         </tbody>
       </table>
       <i
-        class="bi bi-plus-square-fill plus-icon"
+        className="bi bi-plus-square-fill plus-icon"
         style={{ cursor: "pointer" }}
         onClick={addRecord}
       ></i>
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,10 +20,12 @@ const firebaseConfig = {
      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
      appId: process.env.REACT_APP_FIREBASE_APP_ID,
      measurementId: process.env.REACT_APP_FIREBASE_measurementId,
+     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 export default app;
